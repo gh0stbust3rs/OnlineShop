fab3df0f5798861744bf4a8b12148cf86b52ea61
unknown <Andreas@Lackner-Laptop.(none)>
2012-03-27 22:40:14 +0200

    resource commit

---
+++ src/java/pkgOnlineShop/MainController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgOnlineShop;
+
+/**
+ *
+ * @author Andreas
+ */
+public class MainController {
+    
+    public MainController()
+    {
+       
+    }
+  
+}
