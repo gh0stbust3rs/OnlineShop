8e84a030345d3fe72d37e5a0b0024121cb71b1f5
unknown <Andreas@Lackner-Laptop.(none)>
2012-04-17 09:58:12 +0200

    db update

--- export.sql
+++ export.sql
@@ -1,65 +1,19 @@
 --------------------------------------------------------
---  Datei erstellt -Samstag-April-14-2012   
+--  Datei erstellt -Dienstag-April-17-2012   
 --------------------------------------------------------
-  DROP TABLE "USER"."BESTELLUNG" cascade constraints;
-  DROP TABLE "USER"."KATEGORIE" cascade constraints;
-  DROP TABLE "USER"."PERSON" cascade constraints;
-  DROP TABLE "USER"."PRODUKT" cascade constraints;
-  DROP TABLE "USER"."RECHNUNG" cascade constraints;
-  DROP TABLE "USER"."WARENKORB" cascade constraints;
-  DROP SEQUENCE "USER"."SEQ_BESTELLUNG";
-  DROP SEQUENCE "USER"."SEQ_KATEGORIE";
-  DROP SEQUENCE "USER"."SEQ_PERSON";
-  DROP SEQUENCE "USER"."SEQ_PRODUKT";
-  DROP SEQUENCE "USER"."SEQ_RECHNUNG";
-  DROP SEQUENCE "USER"."SEQ_WARENKORB";
---------------------------------------------------------
---  DDL for Sequence SEQ_BESTELLUNG
---------------------------------------------------------
-
-   CREATE SEQUENCE  "USER"."SEQ_BESTELLUNG"  MINVALUE 0 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 20 CACHE 20 NOORDER  NOCYCLE ;
---------------------------------------------------------
---  DDL for Sequence SEQ_KATEGORIE
---------------------------------------------------------
-
-   CREATE SEQUENCE  "USER"."SEQ_KATEGORIE"  MINVALUE 0 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 40 CACHE 20 NOORDER  NOCYCLE ;
---------------------------------------------------------
---  DDL for Sequence SEQ_PERSON
---------------------------------------------------------
-
-   CREATE SEQUENCE  "USER"."SEQ_PERSON"  MINVALUE 0 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 60 CACHE 20 NOORDER  NOCYCLE ;
---------------------------------------------------------
---  DDL for Sequence SEQ_PRODUKT
---------------------------------------------------------
-
-   CREATE SEQUENCE  "USER"."SEQ_PRODUKT"  MINVALUE 0 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 60 CACHE 20 NOORDER  NOCYCLE ;
---------------------------------------------------------
---  DDL for Sequence SEQ_RECHNUNG
---------------------------------------------------------
-
-   CREATE SEQUENCE  "USER"."SEQ_RECHNUNG"  MINVALUE 0 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 20 CACHE 20 NOORDER  NOCYCLE ;
---------------------------------------------------------
---  DDL for Sequence SEQ_WARENKORB
---------------------------------------------------------
-
-   CREATE SEQUENCE  "USER"."SEQ_WARENKORB"  MINVALUE 0 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 20 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Table BESTELLUNG
 --------------------------------------------------------
 
   CREATE TABLE "USER"."BESTELLUNG" 
-   (	"B_ID" NUMBER(*,0), 
-	"DATUM" DATE, 
-	"KREDITKARTE" VARCHAR2(16 BYTE), 
-	"CVC" VARCHAR2(5 BYTE), 
-	"VALID_MONTH" VARCHAR2(2 BYTE), 
-	"VALID_YEAR" VARCHAR2(2 BYTE), 
-	"P_ID" NUMBER(*,0), 
-	"PR_ID" NUMBER(*,0), 
+   (	"BID" NUMBER(*,0), 
+	"P_NR" NUMBER(*,0), 
+	"ORDERDATE" DATE, 
 	"SHIPPED" DATE, 
-	"QUANTITY" NUMBER(*,0), 
-	"WK_ID" NUMBER(*,0), 
-	"R_ID" NUMBER(*,0)
+	"CREDITCARD" NUMBER(*,0), 
+	"CRC" NUMBER(*,0), 
+	"CMONTH" NUMBER(*,0), 
+	"CYEAR" NUMBER(*,0)
    ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
   STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
@@ -71,6 +25,18 @@
   CREATE TABLE "USER"."KATEGORIE" 
    (	"KAT_ID" NUMBER(*,0), 
 	"KAT_BEZEICHNUNG" VARCHAR2(50 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  DDL for Table ORDERPRODUCTS
+--------------------------------------------------------
+
+  CREATE TABLE "USER"."ORDERPRODUCTS" 
+   (	"PR_ID" NUMBER(*,0), 
+	"BID" NUMBER(*,0), 
+	"MENGE" NUMBER(*,0)
    ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
   STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
@@ -143,8 +109,6 @@
   TABLESPACE "USERS" ;
 REM INSERTING into "USER".BESTELLUNG
 SET DEFINE OFF;
-Insert into "USER".BESTELLUNG (B_ID,DATUM,KREDITKARTE,CVC,VALID_MONTH,VALID_YEAR,P_ID,PR_ID,SHIPPED,QUANTITY,WK_ID,R_ID) values ('2',to_date('14.04.12','DD.MM.RR'),'1234567890123456','123','10','13','47','1',null,'5','8','0');
-Insert into "USER".BESTELLUNG (B_ID,DATUM,KREDITKARTE,CVC,VALID_MONTH,VALID_YEAR,P_ID,PR_ID,SHIPPED,QUANTITY,WK_ID,R_ID) values ('3',to_date('14.04.12','DD.MM.RR'),'1234567890123456','123','10','13','47','2',null,'10','9','0');
 REM INSERTING into "USER".KATEGORIE
 SET DEFINE OFF;
 Insert into "USER".KATEGORIE (KAT_ID,KAT_BEZEICHNUNG) values ('0','PC');
@@ -154,6 +118,8 @@ Insert into "USER".KATEGORIE (KAT_ID,KAT_BEZEICHNUNG) values ('3','Prozessoren')
 Insert into "USER".KATEGORIE (KAT_ID,KAT_BEZEICHNUNG) values ('4','Smartphones');
 Insert into "USER".KATEGORIE (KAT_ID,KAT_BEZEICHNUNG) values ('5','Tablets');
 Insert into "USER".KATEGORIE (KAT_ID,KAT_BEZEICHNUNG) values ('6','Zubehör');
+REM INSERTING into "USER".ORDERPRODUCTS
+SET DEFINE OFF;
 REM INSERTING into "USER".PERSON
 SET DEFINE OFF;
 Insert into "USER".PERSON (P_ID,VORNAME,NACHNAME,STRASSE,HAUSNR,PLZ,ORT,LAND,EMAIL,PASS,ANBIETER) values ('0','Matthias','Webhofer','Dorfstraße','27','9905','Gaimberg','Österreich','webhofer.m@gmx.at','admin','1');
@@ -170,7 +136,7 @@ Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('4
 Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('5','Western Digital 0998cf, 1tb, 2,5", intern','99',null,null,null);
 Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('6','Intel Core i7, Quad-Core, 1,4ghz','249',null,null,null);
 Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('7','SmartCover für iPad, schwarz','39',null,null,null);
-Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('33','Produkt 1','235','45','default.png','PC');
+Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('33','Produkt 1','235','-8754','b72e816a-816c-4b88-996b-5304262fb6d1.png','PC');
 Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('34','Produkt 2','253','4','default.png','Tablets');
 Insert into "USER".PRODUKT (PR_ID,BEZEICHNUNG,PREIS,BESTAND,BILD,KAT) values ('40','Produkt 3','2345','32','e96f0717-c8c9-4a03-b2c5-559ca78dd79e.jpg','Prozessoren');
 REM INSERTING into "USER".RECHNUNG
@@ -178,10 +144,33 @@ SET DEFINE OFF;
 Insert into "USER".RECHNUNG (R_ID,R_DATUM) values ('0',to_date('14.04.12','DD.MM.RR'));
 REM INSERTING into "USER".WARENKORB
 SET DEFINE OFF;
-Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('6','1','33','47','1');
-Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('7','0','33','47','1');
-Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('8','5','1','47','0');
-Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('9','10','2','47','0');
+--------------------------------------------------------
+--  DDL for Index SYS_C004195
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "USER"."SYS_C004195" ON "USER"."BESTELLUNG" ("BID") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  DDL for Index SYS_C004075
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "USER"."SYS_C004075" ON "USER"."KATEGORIE" ("KAT_ID") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  DDL for Index UNIQUECONTRAINTKATEGORIE
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "USER"."UNIQUECONTRAINTKATEGORIE" ON "USER"."KATEGORIE" ("KAT_BEZEICHNUNG") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "USERS" ;
 --------------------------------------------------------
 --  DDL for Index MAILUNIQUE
 --------------------------------------------------------
@@ -210,19 +199,10 @@ Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('9','10'
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
   TABLESPACE "USERS" ;
 --------------------------------------------------------
---  DDL for Index SYS_C004075
+--  DDL for Index SYS_C004109
 --------------------------------------------------------
 
-  CREATE UNIQUE INDEX "USER"."SYS_C004075" ON "USER"."KATEGORIE" ("KAT_ID") 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
-  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
-  TABLESPACE "USERS" ;
---------------------------------------------------------
---  DDL for Index SYS_C004076
---------------------------------------------------------
-
-  CREATE UNIQUE INDEX "USER"."SYS_C004076" ON "USER"."BESTELLUNG" ("B_ID") 
+  CREATE UNIQUE INDEX "USER"."SYS_C004109" ON "USER"."RECHNUNG" ("R_ID") 
   PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
   STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
@@ -237,28 +217,10 @@ Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('9','10'
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
   TABLESPACE "USERS" ;
 --------------------------------------------------------
---  DDL for Index SYS_C004109
---------------------------------------------------------
-
-  CREATE UNIQUE INDEX "USER"."SYS_C004109" ON "USER"."RECHNUNG" ("R_ID") 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 
-  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
-  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
-  TABLESPACE "USERS" ;
---------------------------------------------------------
---  DDL for Index UNIQUECONTRAINTKATEGORIE
---------------------------------------------------------
-
-  CREATE UNIQUE INDEX "USER"."UNIQUECONTRAINTKATEGORIE" ON "USER"."KATEGORIE" ("KAT_BEZEICHNUNG") 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
-  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
-  TABLESPACE "USERS" ;
---------------------------------------------------------
 --  Constraints for Table BESTELLUNG
 --------------------------------------------------------
 
-  ALTER TABLE "USER"."BESTELLUNG" ADD PRIMARY KEY ("B_ID")
+  ALTER TABLE "USER"."BESTELLUNG" ADD PRIMARY KEY ("BID")
   USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
   STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
@@ -278,6 +240,7 @@ Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('9','10'
   STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
   TABLESPACE "USERS"  ENABLE;
+
 --------------------------------------------------------
 --  Constraints for Table PERSON
 --------------------------------------------------------
@@ -307,7 +270,7 @@ Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('9','10'
 --------------------------------------------------------
 
   ALTER TABLE "USER"."RECHNUNG" ADD PRIMARY KEY ("R_ID")
-  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
   STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
   PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
   TABLESPACE "USERS"  ENABLE;
@@ -324,18 +287,18 @@ Insert into "USER".WARENKORB (WK_ID,QUANTITY,PR_ID,P_ID,ACTIVE) values ('9','10'
 --  Ref Constraints for Table BESTELLUNG
 --------------------------------------------------------
 
-  ALTER TABLE "USER"."BESTELLUNG" ADD FOREIGN KEY ("P_ID")
+  ALTER TABLE "USER"."BESTELLUNG" ADD FOREIGN KEY ("P_NR")
 	  REFERENCES "USER"."PERSON" ("P_ID") ENABLE;
- 
-  ALTER TABLE "USER"."BESTELLUNG" ADD FOREIGN KEY ("PR_ID")
+
+--------------------------------------------------------
+--  Ref Constraints for Table ORDERPRODUCTS
+--------------------------------------------------------
+
+  ALTER TABLE "USER"."ORDERPRODUCTS" ADD FOREIGN KEY ("PR_ID")
 	  REFERENCES "USER"."PRODUKT" ("PR_ID") ENABLE;
  
-  ALTER TABLE "USER"."BESTELLUNG" ADD FOREIGN KEY ("WK_ID")
-	  REFERENCES "USER"."WARENKORB" ("WK_ID") ENABLE;
- 
-  ALTER TABLE "USER"."BESTELLUNG" ADD FOREIGN KEY ("R_ID")
-	  REFERENCES "USER"."RECHNUNG" ("R_ID") ENABLE;
-
+  ALTER TABLE "USER"."ORDERPRODUCTS" ADD FOREIGN KEY ("BID")
+	  REFERENCES "USER"."BESTELLUNG" ("BID") ENABLE;
 
 --------------------------------------------------------
 --  Ref Constraints for Table PRODUKT
@@ -369,29 +332,6 @@ begin
 end;
 /
 ALTER TRIGGER "USER"."AFTER_DELETE_WARENKORB" ENABLE;
---------------------------------------------------------
---  DDL for Trigger AFTER_INSERT_BESTELLUNG
---------------------------------------------------------
-
-  CREATE OR REPLACE TRIGGER "USER"."AFTER_INSERT_BESTELLUNG" 
-after insert on bestellung for each row
-declare
-  type t_wk is record (
-    wk_quantity warenkorb.quantity%type,
-    wk_pr_id warenkorb.pr_id%type,
-    wk_p_id warenkorb.p_id%type
-  );
-
-  warenkorb_id int;
-  v_quantity int;
-  v_wk t_wk;
-begin
-  warenkorb_id := :new.wk_id;
-  update warenkorb set active=0 where wk_id=warenkorb_id;
-  select quantity,pr_id,p_id into v_wk from warenkorb where wk_id=warenkorb_id;
-end;
-/
-ALTER TRIGGER "USER"."AFTER_INSERT_BESTELLUNG" ENABLE;
 --------------------------------------------------------
 --  DDL for Trigger AFTER_INSERT_WARENKORB
 --------------------------------------------------------
